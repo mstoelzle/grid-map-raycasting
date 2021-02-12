03250f21f8ab96e67740df90306f58e7370f8e59
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-02-12 16:02:15 +0100

    Update grid_map used to test raycasting

--- tests/test.py
+++ tests/test.py
@@ -18,7 +18,7 @@ m.setRaisimLicenseFile(str(raisim_install_path / "rsc" / "activation.raisim"))
 vantage_point = np.array([0., 0., 1.], dtype=np.double)
 # We define a flat Digital Elevation Map (DEM) at the ground-level
 grid_map = np.zeros((64, 64), dtype=np.double)
-grid_map[0:20, 0:20] = 3
+grid_map[0:30, 0:10] = 3
 # We define the resolution of the grid of the DEM in x and y direction
 grid_resolution = np.array([1, 1], dtype=np.double)
 
@@ -27,7 +27,7 @@ occlusion_mask = m.rayCastGridMap(vantage_point, grid_map, grid_resolution)
 
 # The occlusion mask we expect to receive as the output of the ray-casting
 target_occlusion_mask = np.full((64, 64), False)
-target_occlusion_mask[0:19, 0:19] = True
+target_occlusion_mask[0:30, 0:10] = True
 
 fig, axes = plt.subplots(nrows=1, ncols=2)
 axes[0].set_title("Grid map")
